048a617fa0bd67733d5dbb453fb491745bd910cd
Boban Sijuk <sijukboban@gmail.com>
2023-01-24 23:48:22 +0100

    Solution task 5

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,30 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       INPUT value of x 
+       IF value of x=1
+       OUTPUT Monday
+       ELSE
+       IF value of x=2
+       OUTPUT Tuesday
+       ELSE
+       IF value of x=3
+       OUTPUT Wednesday
+       ELSE
+       IF value of x=4
+       OUTPUT Thursday
+       ELSE
+       IF value of x=5
+       OUTPUT Friday
+       ELSE
+       IF value of x=6
+       OUTPUT Saturday
+       ELSE
+       IF value of x=7
+       OUTPUT Sunday
+       ELSE
+       IF value of x=8
+       OUTPUT Invalid input! Please enter number between 1-7.
+    """
 }
