c77822199b666e2fde8e56d8197fe26ed597f1a5
Boban Sijuk <sijukboban@gmail.com>
2023-01-24 23:18:02 +0100

    Solution task 4

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task4(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT value of number x
+        INPUT value of number y
+        IF x<y
+        OUTPUT number y
+        ELSE 
+        IF y<x
+        OUTPUT number x
+    """
 }
