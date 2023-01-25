de8c606664362382f2d8e6b6033a164884a6ca88
Boban Sijuk <sijukboban@gmail.com>
2023-01-25 13:47:48 +0100

    Solution task 11

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT Temperature in C-n
+        INPUT Temperature in F-m
+        IF Temperature in C-n
+        Calculate (n x 9/5) + 32
+        OUTPUT Temperature in C
+        ELSE
+        IF Temperature in F-m
+        Calculate (m - 32) x 5/9
+        OUTPUT Temperature in F
+    """
 }
