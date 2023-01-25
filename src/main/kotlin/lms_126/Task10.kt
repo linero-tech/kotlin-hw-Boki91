8c3bc05e64385902126c7275c3bef6559b196606
Boban Sijuk <sijukboban@gmail.com>
2023-01-25 12:59:14 +0100

    Solution task 10

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,19 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       INPUT Log in here 
+       INPUT Register here
+       IF Log in here
+       OUTPUT Write your email and password
+       ELSE
+       IF Register here
+       OUTPUT Write your email and create the password
+       INPUT Email and password
+       IF Password < 6
+       OUTPUT Your password needs to contain at least 6 characters. Try again.
+       ELSE
+       OUTPUT You have created an account. Log in here.
+       
+    """
 }
