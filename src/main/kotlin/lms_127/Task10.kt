cde2ac37e2d4c50e452e47ff259e7275ac3c8249
Boban Sijuk <sijukboban@gmail.com>
2023-01-30 14:43:07 +0100

    Solution task 10

--- src/main/kotlin/lms_127/Task10.kt
+++ src/main/kotlin/lms_127/Task10.kt
@@ -1,21 +1,34 @@
 package lms_127
 
 fun task10_1(assessments: String): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = assessments.length
+    return result
 }
 
 fun task10_2(assessments: String): Char {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = assessments.get(3)
+    return result
 }
 
 fun task10_3(assessments: String): Char {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = assessments.get(2)
+    return result
 }
 
 fun task10_4(assessments: String): String {
+    val result = assessments.substring(0,3)
+    return result
+}
+fun main() {
+    val result1 = task10_1("LMHHF")
+    println(result1)
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result2 = task10_2("LMFHMF")
+    println(result2)
+
+    val result3 = task10_3("LMFHM")
+    println(result3)
+
+    val result4 = task10_4("LMFHM")
+    println(result4)
 }
