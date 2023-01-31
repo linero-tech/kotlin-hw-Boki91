66e1ed7a0b85e5219b920d08f543f80bea5b3846
Boban Sijuk <sijukboban@gmail.com>
2023-01-31 10:42:28 +0100

    Solution task 8

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -1,7 +1,7 @@
 package lms_127
 
 fun task8(sentence: String, character: Char): Int {
-    val result = sentence.count{it=='I'}
+    val result = sentence.count{it== character}
     return result
 }
 fun main() {
