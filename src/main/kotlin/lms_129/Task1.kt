d5754ffa1bd7b414a407e0656cded0ab81c9b0b8
Boban Sijuk <sijukboban@gmail.com>
2023-02-16 08:53:41 +0100

    Solution task 1

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -5,13 +5,15 @@ fun task1(a: Int, b: Int): Int {
    var result = 0
 
     if (a>=b) {
-        return result
+       return result
     } else {
         for (i in a..b) {
             result=result+i
         }
     }
+
     return result
+
 }
 fun main() {
     val answer = task1(1,5)
