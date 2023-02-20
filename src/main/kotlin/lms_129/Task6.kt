205c7466aefbf990a134c8077ddad3d2da96892e
Boban Sijuk <sijukboban@gmail.com>
2023-02-20 11:29:15 +0100

    Solution task 6

--- src/main/kotlin/lms_129/Task6.kt
+++ src/main/kotlin/lms_129/Task6.kt
@@ -2,5 +2,18 @@ package lms_129
 
 fun task6(number: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    var temporary = number
+
+    while ((temporary % 10 !==0) && (temporary > 0)) {
+        val digit = temporary % 10
+        result = result * 10 + digit
+        temporary /= 10
+    }
+
+    return result
+}
+fun main () {
+    val result = task6(789)
+     println(result)
 }
