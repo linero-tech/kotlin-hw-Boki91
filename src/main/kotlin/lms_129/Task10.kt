684ae3cb8499ef23a519ff094761d41ac0b4eaf0
Boban Sijuk <sijukboban@gmail.com>
2023-02-23 12:10:42 +0100

    Solution task 10

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -1,6 +1,22 @@
 package lms_129
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 fun task10(password: String): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val pattern: Pattern
+    val matcher: Matcher
+
+    val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+
+    pattern = Pattern.compile(PASSWORD_PATTERN)
+    matcher = pattern.matcher(password)
+    
+    return matcher.matches()
+}
+
+fun main () {
+    val result = task10("Agd$3ujk")
+    println(result)
 }
