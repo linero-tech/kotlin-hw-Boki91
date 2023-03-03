a745667a0a2372bb27adc5242ffdb5b034b205d9
Boban Sijuk <sijukboban@gmail.com>
2023-03-03 12:24:21 +0100

    Solution task 5

--- src/main/kotlin/lms_130/Task5.kt
+++ src/main/kotlin/lms_130/Task5.kt
@@ -2,5 +2,16 @@ package lms_130
 
 fun task5(items: List<Int>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = mutableListOf<Int>()
+
+    items.forEachIndexed {index, number ->
+        result.add(index*number)
+    }
+    
+    return result
+}
+
+fun main () {
+    val result = task5(listOf(1, 5, 11))
+    println(result)
 }
