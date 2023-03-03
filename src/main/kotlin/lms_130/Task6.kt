b1585a4ae8df644351a9b6668072f7ce761ecdf3
Boban Sijuk <sijukboban@gmail.com>
2023-03-03 12:33:39 +0100

    Solution task 6

--- src/main/kotlin/lms_130/Task6.kt
+++ src/main/kotlin/lms_130/Task6.kt
@@ -2,5 +2,20 @@ package lms_130
 
 fun task6(sentence: String): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = ""
+
+    sentence.forEachIndexed { index, letter ->
+        result += if (index % 2 == 0) {
+            letter.uppercaseChar()
+        } else {
+            letter
+        }
+    }
+
+    return result
+}
+
+fun main () {
+    val result = task6("I like Gothenburg")
+    println(result)
 }
