8c650f8ca7e78ea3880945b7c08000c8c3d44e7b
Boban Sijuk <sijukboban@gmail.com>
2023-03-03 10:38:40 +0100

    Solution task 3

--- src/main/kotlin/lms_130/Task3.kt
+++ src/main/kotlin/lms_130/Task3.kt
@@ -2,5 +2,24 @@ package lms_130
 
 fun task3(items: List<Int>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = mutableListOf<Int>()
+    var notDuplicated = mutableListOf<Int>()
+
+    for (number in items) {
+        if (number in notDuplicated) {
+            result.add(number)
+        } else {
+            notDuplicated.add(number)
+        }
+    }
+
+
+
+
+    return result.distinct().toList()
+}
+
+fun main () {
+    val result = task3(listOf(5, 5, 6, 6, 4, 3, 3, 2, 1, 1))
+    println(result)
 }
