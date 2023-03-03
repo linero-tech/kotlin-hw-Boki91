3bddb1cfbf0b2d72624486f213e3121e617ccb72
Boban Sijuk <sijukboban@gmail.com>
2023-03-03 09:38:51 +0100

    Solution task 2

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -2,5 +2,37 @@ package lms_130
 
 fun task2(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+
+    /*          METHOD 1
+    for (index in items.indices) {
+
+       var number = items[index]
+
+       if (index % 2 == 0) {
+           result += number
+       } else {
+           0
+       }
+
+    }
+
+     */
+    //      METHOD 2
+
+    items.forEachIndexed{index, number ->
+       if (index % 2 == 0) {
+           result = result + number
+       } else {
+           0
+       }
+    }
+
+
+    return result
+}
+
+fun main () {
+    val result = task2(listOf(1, 2, 3, 4))
+    println(result)
 }
