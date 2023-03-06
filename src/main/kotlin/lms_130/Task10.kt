71f0b5d19f285ad2df15d658e3eb20260331b54a
Boban Sijuk <sijukboban@gmail.com>
2023-03-06 11:56:18 +0100

    Solution task 10

--- src/main/kotlin/lms_130/Task10.kt
+++ src/main/kotlin/lms_130/Task10.kt
@@ -2,5 +2,19 @@ package lms_130
 
 fun task10(items: List<Int>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = mutableListOf<Int>()
+
+    items.forEach { number ->
+        if (number % 2 == 0) {
+            result.add(number * 2)
+        } else {
+            result.add(number * 3)
+        }
+    }
+
+    return result
+}
+fun main () {
+    val result = task10(listOf(1, 2, 3, 4, 5))
+    println(result)
 }
