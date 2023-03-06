91ec53c8f1e15c94100289a206c604fa73affe01
Boban Sijuk <sijukboban@gmail.com>
2023-03-06 10:21:44 +0100

    Solution task 8

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -2,5 +2,38 @@ package lms_130
 
 fun task8(items: List<Int>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val setItem = items.toMutableSet()
+
+    var firstHighValue = Int.MAX_VALUE * - 1
+    var secondHighValue = Int.MAX_VALUE * - 1
+    var thirdHighValue = Int.MAX_VALUE * - 1
+
+    setItem.forEach{number1 ->
+        if (number1 > firstHighValue) {
+           firstHighValue = number1
+        }
+    }
+
+    val newItem = setItem.minus(firstHighValue)
+
+    newItem.forEach { number2 ->
+        if (number2 > secondHighValue) {
+            secondHighValue = number2
+        }
+    }
+
+    val newItem2 = newItem.minus(secondHighValue)
+
+    newItem2.forEach { number3 ->
+        if (number3 > thirdHighValue) {
+            thirdHighValue = number3
+        }
+    }
+
+
+    return listOf(thirdHighValue, secondHighValue, firstHighValue)
+}
+fun main () {
+    val result = task8(listOf(60, 9, 7, 10))
+    println(result)
 }
