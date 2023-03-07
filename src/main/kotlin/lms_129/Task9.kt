4a2c7f81239706eac434eb0a991ab6920a92ced3
Boban Sijuk <sijukboban@gmail.com>
2023-03-07 11:19:50 +0100

    Solution task 9

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -2,6 +2,7 @@ package lms_129
 import kotlin.math.truncate
 
 fun task9(temperature: String): String {
+/*
 
     var temp = temperature.uppercase()
     val result = if ("F" in temp) {
@@ -15,8 +16,23 @@ fun task9(temperature: String): String {
     else {}
 
     return result.toString()
+
+ */
+
+    val magnitude = temperature.substring(0, temperature.length-1).toDouble()
+    val isCelsius = temperature.contains("C", ignoreCase = true)
+
+    val result = if (isCelsius) {
+        val conversion = (9.0 * magnitude / 5.0) + 32.0
+        "${conversion.toInt()}F"
+    } else {
+        val conversion = (5.0 * (magnitude - 32.0)) / 9.0
+        "${conversion.toInt()}C"
+    }
+
+    return result
 }
 fun main () {
-    val result = task9("-90f")
+    val result = task9("50f")
     println(result)
 }
