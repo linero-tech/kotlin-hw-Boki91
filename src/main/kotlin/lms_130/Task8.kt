4f381160db01900f4906ad64877e0bc4ed457845
Boban Sijuk <sijukboban@gmail.com>
2023-03-07 12:49:37 +0100

    Solution task 8

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -1,39 +1,51 @@
 package lms_130
 
 fun task8(items: List<Int>): List<Int> {
-
+/*
     val setItem = items.toMutableSet()
 
     var firstHighValue = Int.MAX_VALUE * - 1
     var secondHighValue = Int.MAX_VALUE * - 1
     var thirdHighValue = Int.MAX_VALUE * - 1
 
-    setItem.forEach{number1 ->
-        if (number1 > firstHighValue) {
+    setItem.forEach {number1 ->
+        if ( number1 > firstHighValue ) {
            firstHighValue = number1
         }
     }
 
-    val newItem = setItem.minus(firstHighValue)
+    val newItems = setItem.minus(firstHighValue)
 
-    newItem.forEach { number2 ->
+    newItems.forEach { number2 ->
         if (number2 > secondHighValue) {
             secondHighValue = number2
         }
     }
 
-    val newItem2 = newItem.minus(secondHighValue)
+    val newItems2 = newItems.minus(secondHighValue)
 
-    newItem2.forEach { number3 ->
+    newItems2.forEach { number3 ->
         if (number3 > thirdHighValue) {
             thirdHighValue = number3
         }
     }
 
+    return result(thirdHighValue, secondHighValue, FirstHighValue)
 
-    return listOf(thirdHighValue, secondHighValue, firstHighValue)
+
+ */
+
+    var result = mutableListOf(items[0], items[1], items[2]).sorted().toMutableList()
+    items.forEach { item ->
+        if (item > result[0] && item !in result) {
+           result[0] = item
+            result = result.sorted().toMutableList()
+        }
+    }
+
+    return result.toSet().toList()
 }
 fun main () {
-    val result = task8(listOf(60, 9, 7, 10))
+    val result = task8(listOf(2, 2, 1))
     println(result)
 }
